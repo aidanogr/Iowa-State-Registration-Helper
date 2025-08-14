b485ee3c750355c3852a33b0fa1ad8770364aebf
Aidan O'Grady <aidanhenryrc@gmail.com>
2025-08-13 22:39:48 -0500

    TODO: create position absolute but not

--- common/src/main/java/com/ogradytech/registration/Utilities/CalendarContainer.java
+++ common/src/main/java/com/ogradytech/registration/Utilities/CalendarContainer.java
@@ -1,11 +1,36 @@
 package com.ogradytech.registration.Utilities;
 
+import java.util.ArrayList;
+
+import com.codename1.ui.Component;
 import com.codename1.ui.Container;
 import com.codename1.ui.List;
+import com.codename1.ui.layouts.Layout;
+import com.codename1.ui.table.TableLayout;
 
-public class CalendarContainer {
+public class CalendarContainer  {
 
-	public Container container;
-	public List<CalendarItem> courseItems;
 	
+	// ============== GUI ELEMENTS =============== //
+	public Container container;
+	public Container[] days;
+	public ArrayList<CalendarItem> courseItems;
+	
+	public CalendarContainer() {
+		container = new Container();
+		container.setUIID("CalendarContainer");
+		
+		TableLayout daysOfWeekCalendarLayout = new TableLayout(1, 5);
+		container.setLayout(daysOfWeekCalendarLayout);
+		
+		days = new Container[5];	//we're just gonna pretend like saturday and sunday dont exist (what are the odds you have 2 classes on the weekend)
+		for(int i = 0; i < days.length; i++) {
+			days[i] = new Container();
+			days[i].setUIID("DayContainer");
+			days[i].setLayout(new ConstraintLayout()); //TODO implement this.
+		}
+
+		courseItems = new ArrayList<CalendarItem>();
+	}
+
 }
--- common/src/main/java/com/ogradytech/registration/Utilities/CalendarItem.java
+++ common/src/main/java/com/ogradytech/registration/Utilities/CalendarItem.java
@@ -4,20 +4,31 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.codename1.ui.Button;
 import com.ogradytech.registration.Utilities.MeetingInfo.Days;
 
 public class CalendarItem {
 
+	private static int allColors[] = {0x000, 0x005C09, 0x540085, 0x850009, 0x850098, 0x000098, 0xCE7800, 0x00787A, 0x007185, 0xFF6200, };
+	private static int currentColorIndex = 0;
 
 	private String courseName;
 	private Map<String, MeetingInfo> sectionMeetingInfo;
 	private String currentSection; 	//could be number or letter,
+	public Button button;
 
 	
 	public CalendarItem(String courseName) {
 		this.courseName = courseName;
 		this.sectionMeetingInfo = new HashMap<>();
 		currentSection = "";
+		button = new Button(courseName);
+		button.setUIID("ClassButton");
+		button.getAllStyles().setBgColor(allColors[currentColorIndex]);
+		currentColorIndex++;
+		if(currentColorIndex >= allColors.length) {
+			currentColorIndex = 0;
+		}
 	}
 	
 	public void addMeetingInfo(String section, short hour, short minute, ArrayList<Days> days) {
